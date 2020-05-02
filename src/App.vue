9c84c43e5469e4ca5fda905efc112fa501f3b5f8
Hasan Basri <hasanbasri1493@gmail.com>
2020-05-03 01:35:42 +0700

    add dummy listChats add filter listChats

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <div class="appPraInside">
             <div tabindex="-1" class="app h70RQ two">
-                <SidePanel></SidePanel>
+                <SidePanel :listChats="listChats"></SidePanel>
                 <MainPanel></MainPanel>
             </div>
         </div>
@@ -15,6 +15,121 @@
 
     export default {
         name: 'App',
+        data: () => ({
+            listChats: [
+                {
+                    chatID: 1,
+                    name: 'Una',
+                    photo: 'https://picsum.photos/id/1/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 2,
+                    name: 'Una2',
+                    photo: 'https://picsum.photos/id/31/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 3,
+                    name: 'Una3',
+                    photo: 'https://picsum.photos/id/14/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 4,
+                    name: 'Una4',
+                    photo: 'https://picsum.photos/id/9/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 5,
+                    name: 'Una5',
+                    photo: 'https://picsum.photos/id/345/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 6,
+                    name: 'Una',
+                    photo: 'https://picsum.photos/id/1/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 7,
+                    name: 'Una2',
+                    photo: 'https://picsum.photos/id/31/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 8,
+                    name: 'Una3',
+                    photo: 'https://picsum.photos/id/14/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 9,
+                    name: 'Una4',
+                    photo: 'https://picsum.photos/id/9/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 10,
+                    name: 'Una5',
+                    photo: 'https://picsum.photos/id/345/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },{
+                    chatID: 11,
+                    name: 'Una5',
+                    photo: 'https://picsum.photos/id/345/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 12,
+                    name: 'Una',
+                    photo: 'https://picsum.photos/id/1/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 13,
+                    name: 'Una2',
+                    photo: 'https://picsum.photos/id/31/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 14,
+                    name: 'Una3',
+                    photo: 'https://picsum.photos/id/14/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 15,
+                    name: 'Una4',
+                    photo: 'https://picsum.photos/id/9/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+                {
+                    chatID: 16,
+                    name: 'Una5',
+                    photo: 'https://picsum.photos/id/345/200/200',
+                    lastChatT: '04:12',
+                    lastChatM: 'lalala skksskjeje'
+                },
+            ]
+        }),
         components: {
             SidePanel,
             MainPanel
